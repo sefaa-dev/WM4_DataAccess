be0623aea4f06fc784fd4efd63feac6412724056
sefaa-dev <sefayilmaz582@gmail.com>
2021-12-02 09:38:12 +0300

    data

---
+++ Nort_DbFront/KategoryForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Nort_DbFront
+{
+    partial class KategoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstCategory = new System.Windows.Forms.ListBox();
+            this.txtKategoryName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstCategory
+            // 
+            this.lstCategory.FormattingEnabled = true;
+            this.lstCategory.ItemHeight = 15;
+            this.lstCategory.Location = new System.Drawing.Point(22, 57);
+            this.lstCategory.Name = "lstCategory";
+            this.lstCategory.Size = new System.Drawing.Size(219, 349);
+            this.lstCategory.TabIndex = 0;
+            this.lstCategory.SelectedIndexChanged += new System.EventHandler(this.lstCategory_SelectedIndexChanged);
+            // 
+            // txtKategoryName
+            // 
+            this.txtKategoryName.Location = new System.Drawing.Point(257, 28);
+            this.txtKategoryName.Name = "txtKategoryName";
+            this.txtKategoryName.Size = new System.Drawing.Size(220, 23);
+            this.txtKategoryName.TabIndex = 1;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(257, 57);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(220, 175);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(257, 273);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(67, 64);
+            this.btnKaydet.TabIndex = 3;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(366, 273);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(67, 64);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "Guncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(475, 273);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(67, 64);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(22, 28);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(219, 23);
+            this.txtAra.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Kategori Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Açıklama";
+            // 
+            // KategoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtKategoryName);
+            this.Controls.Add(this.lstCategory);
+            this.Name = "KategoryForm";
+            this.Text = "KategoryForm";
+            this.Load += new System.EventHandler(this.KategoryForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCategory;
+        private System.Windows.Forms.TextBox txtKategoryName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Nort_DbFront/KategoryForm.cs
@@ -0,0 +1,63 @@
+﻿using Nort_DbFront.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nort_DbFront
+{
+    public partial class KategoryForm : Form
+    {
+        public KategoryForm()
+        {
+            InitializeComponent();
+        }
+        NorthwindContext _dbContext = new NorthwindContext();
+        private void KategoryForm_Load(object sender, EventArgs e)
+        {
+            ListeyiDoldur();
+        }
+        Category _selectedCategory;
+        private void ListeyiDoldur()
+        {
+            lstCategory.DataSource = _dbContext.Categories.ToList();
+            lstCategory.DisplayMember = "CategoryName";
+        }
+
+        private void lstCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstCategory.SelectedItem == null) return;
+            _selectedCategory = (Category)lstCategory.SelectedItem;
+
+            txtKategoryName.Text = _selectedCategory.CategoryName;
+            txtDescription.Text = _selectedCategory.Description;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var yeni = new Category
+                {
+                    CategoryName = txtKategoryName.Text,
+                    Description = txtDescription.Text,
+                };
+                _dbContext.Categories.Add(yeni);
+
+                int result = _dbContext.SaveChanges();
+                ListeyiDoldur();
+               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
