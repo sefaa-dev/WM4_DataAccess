9d92af7f71f50064e4c9af97dadad8f72bf922d4
sefaa-dev <sefayilmaz582@gmail.com>
2021-12-02 10:57:21 +0300

    fhfd

---
+++ Nort_DbFront/UrunForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Nort_DbFront
+{
+    partial class UrunForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.cmbKategory = new System.Windows.Forms.ComboBox();
+            this.cmbSupplier = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.nUnitPrice = new System.Windows.Forms.NumericUpDown();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUnitPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.ItemHeight = 15;
+            this.lstProducts.Location = new System.Drawing.Point(12, 72);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(190, 364);
+            this.lstProducts.TabIndex = 0;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(246, 43);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(125, 23);
+            this.txtProductName.TabIndex = 1;
+            // 
+            // cmbKategory
+            // 
+            this.cmbKategory.FormattingEnabled = true;
+            this.cmbKategory.Location = new System.Drawing.Point(412, 43);
+            this.cmbKategory.Name = "cmbKategory";
+            this.cmbKategory.Size = new System.Drawing.Size(151, 23);
+            this.cmbKategory.TabIndex = 2;
+            // 
+            // cmbSupplier
+            // 
+            this.cmbSupplier.FormattingEnabled = true;
+            this.cmbSupplier.Location = new System.Drawing.Point(412, 82);
+            this.cmbSupplier.Name = "cmbSupplier";
+            this.cmbSupplier.Size = new System.Drawing.Size(151, 23);
+            this.cmbSupplier.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(246, 133);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Devam Etmiyor";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // nUnitPrice
+            // 
+            this.nUnitPrice.DecimalPlaces = 2;
+            this.nUnitPrice.Location = new System.Drawing.Point(246, 83);
+            this.nUnitPrice.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nUnitPrice.Name = "nUnitPrice";
+            this.nUnitPrice.Size = new System.Drawing.Size(120, 23);
+            this.nUnitPrice.TabIndex = 5;
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(12, 43);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(181, 23);
+            this.txtAra.TabIndex = 6;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(230, 193);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(83, 57);
+            this.btnEkle.TabIndex = 7;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(368, 193);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(83, 57);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(504, 193);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(83, 57);
+            this.btnSil.TabIndex = 9;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // UrunForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.nUnitPrice);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.cmbSupplier);
+            this.Controls.Add(this.cmbKategory);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.lstProducts);
+            this.Name = "UrunForm";
+            this.Text = "UrunForm";
+            this.Load += new System.EventHandler(this.UrunForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nUnitPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.ComboBox cmbKategory;
+        private System.Windows.Forms.ComboBox cmbSupplier;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown nUnitPrice;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
