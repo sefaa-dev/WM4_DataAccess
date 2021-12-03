ad500bb2ae89de7619ab425405fecaeb0d67d2e0
sefaa-dev <sefayilmaz582@gmail.com>
2021-12-03 08:40:42 +0300

    SFSDSFS

--- North_DbFirst/Program.cs
+++ North_DbFirst/Program.cs
@@ -14,7 +14,7 @@ namespace North_DbFirst
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SiparisForm());       
+            Application.Run(new SiparisForm());        
         } 
     }
 }
